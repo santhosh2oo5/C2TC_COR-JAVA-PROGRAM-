286801b492bb822f238975fbe2c676d455c57103
Windows <Windows@SANDY.mshome.net>
2025-06-12 12:29:39 +0530

    update

---
+++ C2TC_Corejavaprogram/src/day1/additional.java
@@ -0,0 +1,20 @@
+package day1;
+import java.util.Scanner;
+
+public class additional {
+
+	public static void main(String[] args) {
+		int a,b,c;
+		Scanner sc;
+		sc = new Scanner(System.in);
+		
+		System.out.println("enter a:");
+		a = sc.nextInt();
+				
+	    System.out.println("enter b:");
+		b = sc.nextInt();
+		c=a+b;
+	System.out.println("addition of a="+a+" and b="+b+" is  "+c);
+	}
+
+}
