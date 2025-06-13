0a886f723eb17bf397dd0bd2df62019526ba4d43
Windows <Windows@SANDY>
2025-06-13 10:50:30 +0530

    new programs

---
+++ C2TC_Corejavaprogram/src/day1/IdentifiersDemo.java
@@ -0,0 +1,14 @@
+package day1;
+
+public class IdentifiersDemo {
+	public static void main(String[] args) {
+		
+		
+		int $number = 20; //starts with $
+		System.out.println("value of the number variable is : "+ $number);
+	
+		String studentName="Aniket"; 
+		System.out.println("value is : "+ studentName);
+	}
+
+}
