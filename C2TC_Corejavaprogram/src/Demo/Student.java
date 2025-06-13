db7b51fa5212cc55425fd0ebca7c4a9e9bf349d4
Windows <Windows@SANDY>
2025-06-13 12:33:20 +0530

    new update 1

---
+++ C2TC_Corejavaprogram/src/Demo/Student.java
@@ -0,0 +1,23 @@
+package Demo;
+
+public class Student {
+	public static void main(String[] args) {
+		Studentt st   =new Studentt();
+		st.setId(101);
+		st.setName("john");
+		st.setAge(20);
+		
+		System.out.println(st);
+		
+		
+		Studentt st1   =new Studentt();
+		st1.setId(102);
+		st1.setName("johnm");
+		st1.setAge(21);
+		
+		System.out.println(st1);
+		
+		
+	}
+
+}
